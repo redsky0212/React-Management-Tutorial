9e03e2fca932c525686dc6da0ac1bd292ea6f3a1
redsky <hyun0238@gmail.com>
2019-08-02 14:52:09 +0900

    설명내용수정 및 App.js파일 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>관리 프로그램</h2>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="gray-background">
+        <img src={logo} lat="logo" />
+        <h2>관리 프로그램</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
